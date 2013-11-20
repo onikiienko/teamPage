a7f6fd06784c01f9cd4f98ab73e84e5d80ad7f4a
AndreyGeonya <a.geonya@gmail.com>
2013-11-20 19:28:53 +0200

    jsteam fixed

--- js/conf.js
+++ js/conf.js
@@ -10,19 +10,19 @@ commandMenu.id = "member-menu";
 commandMenu.innerHTML = '<h1 class="dg-popup-header-title">Команда</h1>';
 
 newMember("a.geonya", "Андрей", "разработчики", "может не спать 2 суток");
-newMember("d.lutsyk", "Дмитрий", "разработчики", "спокойствие");
-newMember("b.onikienko", "Богдан", "тестировщики", "моет руки перед релизом");
-newMember("p.gajdamak", "Павел", "тестировщики", "повелитель котиков");
-newMember("a.borodatov", "Антон", "разработчики", "повелитель котиков");
-newMember("ja.voloshyuk", "Ярослав", "разработчики", "повелитель котиков");
+newMember("d.lutsyk", "Дмитрий", "разработчики", "спокойствие богомола");
+newMember("b.onikienko", "Богдан", "тестировщики", "взял мак в ипотеку");
+newMember("p.gajdamak", "Павел", "тестировщики", "повеливает котиками");
+newMember("a.borodatov", "Антон", "разработчики", "git pull --jack daniels --repeat");
+newMember("ja.voloshyuk", "Ярослав", "разработчики", "знает дорогу в оперетту");
 newMember("a.kim", "Алексей", "добрая и общительная поддержка", "железные нервы");
-newMember("new", "Новенький", "разработчики", "<a href='http://job.2gis.ru/vacancy/kiev/id/474/'>тут</a>");
+newMember("new", "Присоединяйся!", "разработчики", "<a target='_blank' href='http://job.2gis.ru/vacancy/kiev/id/474/'>тут</a>");
 
 var header1 = '<h1 class="dg-popup-header-title">Процессы</h1>';
 
 var body1 = '<ul class="building-callout__list"> \
-<li class="building-callout__list-item">Живем Scrum’ом (планирования, презентации, ретро, дейли-митинги) </li> \
-<li class="building-callout__list-item">Пишем unit-тесты, делаем CodeReview и используем Continuous Intergation </li> \
+<li class="building-callout__list-item">Живем Scrum’ом (планирования, презентации, ретроспективы, дейли-митинги) </li> \
+<li class="building-callout__list-item">Пишем unit-тесты, делаем Code Review, используем Continuous Intergation </li> \
 <li class="building-callout__list-item">Занимаемся Continuous Delivery’нгом </li> \
 <li class="building-callout__list-item">Дышим автоматизацией (Mocha, Selenium WebDriver) </li> \
 </ul>';
@@ -43,19 +43,19 @@ var body3 = commandCards;
 var header4 = '<h1 class="dg-popup-header-title">Задачи</h1>';
 
 var body4 = '<ul class="building-callout__list"> \
-<li class="building-callout__list-item">Колбасим API карты 2ГИС </li> \
+<li class="building-callout__list-item">Колбасим API карт 2GIS </li> \
 <li class="building-callout__list-item">Любим Canvas, SVG, WebGL, поглядываем на GLSL, WebGL фреймворки, мыслим draw call’ами </li> \
 <li class="building-callout__list-item">Работаем с Leaflet</li> \
-<li  class="building-callout__list-item>Интегрируемся с внутренними сервисами компании </li> \
+<li  class="building-callout__list-item">Интегрируемся с внутренними сервисами компании </li> \
 <li class="building-callout__list-item">Ворочаем кучей данных на клиенте </li> \
-<li class="building-callout__list-item">Ускоряем отрисовки в браузерах и скорость загрузки API карт 2ГИС </li> \
+<li class="building-callout__list-item">Ускоряем отрисовки в браузерах и скорость загрузки API карт 2GIS </li> \
 </ul>';
 
 var header5 = '<h1 class="dg-popup-header-title">Плюшки</h1>';
 
 var body5 = '<ul class="building-callout__list"> \
 <li class="building-callout__list-item">Двухэтажный офис на последних этажах высотного здания с замечательным видом на центр Киева </li> \
-<li class="building-callout__list-item">Кормим вкусными завтраками, катаем на самокате(круглый год), усаживаем в мягкие груши </li> \
-<li class="building-callout__list-item">Ездим на it-конференции</li> \
-<li  class="building-callout__list-item>Делаем YiiConf, LambaDay, StartupDay, подслушиваем DevDay </li> \
+<li class="building-callout__list-item">Кормим вкусными завтраками, катаем на самокате (круглый год), усаживаем в мягкие груши </li> \
+<li class="building-callout__list-item">Ездим на IT-конференции</li> \
+<li  class="building-callout__list-item>Делаем &lambda; day, StartupDay, подслушиваем DevDay </li> \
 </ul>';
--- js/map.js
+++ js/map.js
@@ -3,8 +3,8 @@ var map,
 
 L.onLoad(function () {
 	map = new L.Map('map', {
-    	"center": new L.LatLng(0, 0),
-    	"zoom": 3,
+    	"center": new L.LatLng(15, 0),
+    	"zoom": 4,
     	"dgGeoclicker": false
 	});
 
@@ -13,13 +13,13 @@ L.onLoad(function () {
 		minZoom: 3
 	}).addTo(map);
 
+    map.removeLayer(map._layers[22]);
+
 	createMarker(0, 0, header1, body1, 430, 300);
 	createMarker(-18.14585, -13.35937, header2, body2, 430, 300);
 	createMarker(6.83917, 22.85156, header4, body4, 430, 300);
 	createMarker(-14.94478, 20.74219, header5, body5, 430, 300);
-	var nextMarker = createMarker(20.30342, -16.17187, header3, body3, 490, 300);
-    nextMarker.on('popupopen', setEvents);  	
+	var nextMarker = createMarker(12.30342, -13.57187, header3, body3, 490, 300);
+    nextMarker.on('popupopen', setEvents);
+    nextMarker.openPopup();
 });
-
-
-
